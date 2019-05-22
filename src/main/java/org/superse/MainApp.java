4e83ad07cfeae7bea952cb47e775d1f8fe8c329b
hienlh <hienlh1298@gmail.com>
2019-05-22 10:59:23 +0700

    Initialize mvc javafx Project

---
+++ src/main/java/org/superse/MainApp.java
@@ -0,0 +1,24 @@
+package org.superse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.superse.SignInScreen.SignInScreen;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Scene scene = new Scene(new SignInScreen());
+
+        primaryStage.setTitle("JavaFX and Gradle");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
